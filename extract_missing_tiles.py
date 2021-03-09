a029781b9c732f5e798cda611f723fe83eba7c32
nicolas-sitylb <nbl@ylb.ch>
2021-03-09 09:35:43 +0000

    Remove useless code.

--- extract_missing_tiles.py
+++ extract_missing_tiles.py
@@ -8,13 +8,9 @@ def extract_missing_tiles():
     file_existing = 'ortho.csv'
     
     with open(file_existing) as fe, open(file_all) as fa:
-        data_fe = fe.readlines()
         data_fa = fa.readlines()
-
-    with open(file_existing) as fe:
         dataFe = fe.read()
 
-    data_fe = [row_fe.replace('-', '_') for row_fe in data_fe]
     data_fa = [row_fa.strip('\n') for row_fa in data_fa]
     
     matchings = [row_fa for row_fa in data_fa if row_fa.replace('_','-') in dataFe]
